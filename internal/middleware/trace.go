f8d1816ba4914a1dcff91044d0e129b6b3db7256
wwwdotsheng <83939226+wwwdotsheng@users.noreply.github.com>
2026-02-19 20:46:33 +0800

    指标监控添加rate,errors,duration记录

--- internal/middleware/trace.go
+++ internal/middleware/trace.go
@@ -2,12 +2,21 @@ package middleware
 
 import (
 	"e-commerce/pkg/clog"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
+)
+
+var (
+	meter                = otel.GetMeterProvider().Meter("e-commerce-api")
+	httpRequestsTotal, _ = meter.Int64Counter("http_requests_total")
+	httpDuration, _      = meter.Float64Histogram("http_request_duration_seconds")
 )
 
 func TraceMiddleware(serviceName string) gin.HandlerFunc {
@@ -18,6 +27,11 @@ func RequestLogMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		start := time.Now()
 		path := c.Request.URL.Path
+		route := c.FullPath()
+		// 当出现404的情况
+		if route == "" {
+			route = "unknow"
+		}
 		query := c.Request.URL.RawQuery
 
 		span := trace.SpanFromContext(c.Request.Context())
@@ -31,12 +45,21 @@ func RequestLogMiddleware() gin.HandlerFunc {
 
 		c.Next()
 
+		ctx := c.Request.Context()
 		latency := time.Since(start)
+		attrs := metric.WithAttributes(
+			attribute.String("method", c.Request.Method),
+			attribute.String("route", route),
+			attribute.Int("status", c.Writer.Status()),
+		)
+		httpRequestsTotal.Add(ctx, 1, attrs)
+		httpDuration.Record(ctx, latency.Seconds(), attrs)
 
-		clog.L(c.Request.Context()).Info("HTTP Request",
+		clog.L(ctx).Info("HTTP Request",
 			zap.Int("status", c.Writer.Status()),
 			zap.String("method", c.Request.Method),
 			zap.String("path", path),
+			zap.String("full-path", route),
 			zap.String("query", query),
 			zap.String("ip", c.ClientIP()),
 			zap.Duration("latency", latency),
